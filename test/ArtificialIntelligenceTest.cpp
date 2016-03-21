7b63740f35cda8ea9e23268b0ef3d9b080cfe4a6
Klapeyron <t.m@o2.pl>
2016-03-21 11:18:05 +0100

    AI test implementation

--- test/ArtificialIntelligenceTest.cpp
+++ test/ArtificialIntelligenceTest.cpp
@@ -4,7 +4,7 @@
 #include "GameControlMock.hpp"
 #include "TableSnapshot.hpp"
 
-TEST (ArtificialIntelligenceTest, missingSequenceCardShouldBeTakenFromStack )
+TEST (ArtificialIntelligenceTest, missingSequenceCardShouldBeTakenFromStack)
 {
   TableSnapshot tableSnapshot;
 
@@ -15,7 +15,7 @@ TEST (ArtificialIntelligenceTest, missingSequenceCardShouldBeTakenFromStack )
   };
 
   tableSnapshot.stackCard = Card(Card_Figure::SEVEN, Card_Color::CLUB);
-
+  
   GameControlMock gameControl;
   ArtificialIntelligence artificialIntelligence(gameControl);
 
@@ -27,3 +27,48 @@ TEST (ArtificialIntelligenceTest, missingSequenceCardShouldBeTakenFromStack )
   artificialIntelligence.onUpdate(tableSnapshot);
 }
 
+TEST (ArtificialIntelligenceTest, cardOnStackIsNotMissingInSequence_cardFromHiddenStackShouldBeTaken)
+{
+  TableSnapshot tableSnapshot;
+
+  tableSnapshot.playerCards = {
+    Card(Card_Figure::SEVEN, Card_Color::SPADE),
+    Card(Card_Figure::SEVEN, Card_Color::DIAMOND),
+    Card(Card_Figure::SEVEN, Card_Color::HEART)
+  };
+
+  tableSnapshot.stackCard = Card(Card_Figure::TWO, Card_Color::SPADE);
+  
+  GameControlMock gameControl;
+  ArtificialIntelligence artificialIntelligence(gameControl);
+
+  EXPECT_CALL(gameControl,pickCardFromStack())
+      .Times(0);
+  EXPECT_CALL(gameControl,pickCardFromHiddenStack())
+      .Times(1);
+
+  artificialIntelligence.onUpdate(tableSnapshot);
+}
+
+TEST (ArtificialIntelligenceTest, cardOnStackIsNotMissingInSequence_cardFromHiddenStackShouldBeTaken)
+{
+  TableSnapshot tableSnapshot;
+
+  tableSnapshot.playerCards = {
+    Card(Card_Figure::SEVEN, Card_Color::SPADE),
+    Card(Card_Figure::SEVEN, Card_Color::DIAMOND),
+    Card(Card_Figure::SEVEN, Card_Color::HEART)
+  };
+
+  tableSnapshot.stackCard = Card(Card_Figure::TWO, Card_Color::SPADE);
+  
+  GameControlMock gameControl;
+  ArtificialIntelligence artificialIntelligence(gameControl);
+
+  EXPECT_CALL(gameControl,pickCardFromStack())
+      .Times(0);
+  EXPECT_CALL(gameControl,pickCardFromHiddenStack())
+      .Times(1);
+
+  artificialIntelligence.onUpdate(tableSnapshot);
+}
