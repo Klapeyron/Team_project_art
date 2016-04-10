4680c5a08f80490ab2a8ebee1cfe9f49623020cf
Klapeyron <t.m@o2.pl>
2016-04-10 21:32:31 +0200

    TableAnalyzer template

---
+++ include/TableAnalyzer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <opencv/cv.hpp>
+
+const double THRESHOLD = 0.87;
+const int MATCH_METHOD = CV_TM_CCOEFF_NORMED;
+using Image = cv::Mat;
+
+class TableAnalyzer {
+  bool containsImageTemplate(Image const& image, Image const& templ);
+ public:
+};
