115cb5d31edb13ba9ad2ffd98a4fa7714b40b73b
klapeyron <t.m@o2.pl>
2016-04-11 08:10:10 +0200

    Constexpr for Card_Color and Card_Figure base range loops

--- include/Card.hpp
+++ include/Card.hpp
@@ -3,15 +3,15 @@
 enum class Card_Color : unsigned int {None = 0, SPADE, HEART, CLUB, DIAMOND}; 
 enum class Card_Figure : unsigned int {None = 0, A, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
 
-inline Card_Color operator++(Card_Color & x) { return x = static_cast<Card_Color>(static_cast<unsigned int>(x) + 1); }
-inline Card_Color operator*(Card_Color x) {return x;}
-inline Card_Color begin(Card_Color x) {return Card_Color::SPADE;}
-inline Card_Color end(Card_Color x) {return static_cast<Card_Color>(static_cast<unsigned int>(Card_Color::DIAMOND) + 1);}
+constexpr inline Card_Color operator++(Card_Color & x) { return x = static_cast<Card_Color>(static_cast<unsigned int>(x) + 1); }
+constexpr inline Card_Color operator*(Card_Color x) {return x;}
+constexpr inline Card_Color begin(Card_Color x) {return Card_Color::SPADE;}
+constexpr inline Card_Color end(Card_Color x) {return static_cast<Card_Color>(static_cast<unsigned int>(Card_Color::DIAMOND) + 1);}
 
-inline Card_Figure operator++(Card_Figure & x) {return x = static_cast<Card_Figure>(static_cast<unsigned int>(x) + 1);}
-inline Card_Figure operator*(Card_Figure x) {return x;}
-inline Card_Figure begin(Card_Figure x) {return Card_Figure::A;}
-inline Card_Figure end(Card_Figure x) {return static_cast<Card_Figure>(static_cast<unsigned int>(Card_Figure::KING) + 1);}
+constexpr inline Card_Figure operator++(Card_Figure & x) {return x = static_cast<Card_Figure>(static_cast<unsigned int>(x) + 1);}
+constexpr inline Card_Figure operator*(Card_Figure x) {return x;}
+constexpr inline Card_Figure begin(Card_Figure x) {return Card_Figure::A;}
+constexpr inline Card_Figure end(Card_Figure x) {return static_cast<Card_Figure>(static_cast<unsigned int>(Card_Figure::KING) + 1);}
 
 class Card {
   Card_Color color;
