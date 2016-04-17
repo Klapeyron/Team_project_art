2cf04b0c05dfc363088c5bfbdf0faf226c9e2cfc
209255 <209255@student.pwr.edu.pl>
2016-04-17 11:05:46 +0200

    change constexpr to const

--- include/ButtonsSnapshot.hpp
+++ include/ButtonsSnapshot.hpp
@@ -8,10 +8,12 @@
 const std::string END_GAME_BUTTON = "endgame";
 const std::string OK_BUTTON = "ok";
 const std::string KNOCK_KNOCK_BUTTON = "knock";
+const std::string STACK_BUTTON = "stack";
+const std::string HIDDEN_STACK_BUTTON = "hidden_stack";
 
 class ButtonsSnapshot
 {
   std::map<std::string,Position> buttons;
  public:
-  Position getButtonPosition(std::string const& buttonName);
+  static Position getButtonPosition(std::string const& buttonName);
 };
--- include/GameControl.hpp
+++ include/GameControl.hpp
@@ -2,13 +2,19 @@
 
 #include "Card.hpp"
 #include "IGameControl.hpp"
+#include "Position.hpp"
+#include "ButtonsSnapshot.hpp"
 
-class GameControl :public IGameControl {
- public:
+class GameControl :public IGameControl 
+{
+private:
+  void makeMove(const Position & position);
+public:
   void pickCardFromHiddenStack();
   void pickCardFromStack();
   void throwMyCard(Card const&);
   void endGame();
   void touchCard(Card const&);
   void pressOK();
+  
 };
