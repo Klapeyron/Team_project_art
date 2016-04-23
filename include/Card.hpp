d9a79b9aa2760087cfbcf6e20a20e46abf93437c
Klapeyron <t.m@o2.pl>
2016-04-23 13:05:20 +0200

    Moved operators into test disrectory

--- include/Card.hpp
+++ include/Card.hpp
@@ -29,24 +29,3 @@ class Card :public Position {
   bool operator ==(const Card& card) const;
   bool operator !=(const Card& card) const;
 };
-
-inline std::ostream& operator<<(::std::ostream& os, const Card_Color& color) {
-  if(Card_Color::SPADE == color) return os << "SPADE";
-  if(Card_Color::HEART == color) return os << "HEART";
-  if(Card_Color::CLUB == color) return os << "CLUB";
-  if(Card_Color::DIAMOND == color) return os << "DIAMOND";
-  if(Card_Color::None == color) return os << "None";
-}
-
-inline std::ostream& operator<<(::std::ostream& os, const Card_Figure& figure) {
-  if(Card_Figure::A == figure) return os << "A";
-  if(Card_Figure::JACK == figure) return os << "JACK";
-  if(Card_Figure::QUEEN == figure) return os << "QUEEN";
-  if(Card_Figure::KING == figure) return os << "KING";
-  if(Card_Figure::None == figure) return os << "None";
-  return os << static_cast<int> (figure);
-}
-
-inline std::ostream& operator<<(::std::ostream& os, const Card& card) {
-  return os << card.getFigure() << " " << card.getColor() << std::endl;
-}
--- include/TableSnapshot.hpp
+++ include/TableSnapshot.hpp
@@ -8,10 +8,3 @@ struct TableSnapshot {
   Card stackCard;
   bool myMove = true;
 };
-
-inline std::ostream& operator<<(::std::ostream& os, const TableSnapshot& snapshot) {
-  os << "Player cards: ";
-  for(auto const& playerCard : snapshot.playerCards)
-    os << playerCard << " | ";
-  return os << "stackCard: " << snapshot.stackCard << " | " << "myMove: " << snapshot.myMove;
-}
