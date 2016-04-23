af75b128fdc9a5363c53ecaffc32fbe902c19079
Klapeyron <t.m@o2.pl>
2016-04-23 13:17:41 +0200

    Merged detection system tests

--- test/DetectionSystemTest.cpp
+++ test/DetectionSystemTest.cpp
@@ -4,18 +4,31 @@
 #include "TableObserverMock.hpp"
 #include "SnapshotMatcher.hpp"
 
-TEST (DetectionSystemTest, heartPlayerCardsDetectionTest)
+TEST (DetectionSystemTest, 1366x768DetectionTest)
 {
-  const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/tableSnapshot.tiff";
+  const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/tableSnapshot1366x768.tiff";
   DetectionSystem detectionSystem (tableSnapshotFilePath);
   TableObserverMock tableObserver;
   detectionSystem.registerOberver(tableObserver);
  
   TableSnapshot output;
-  output.playerCards.emplace_back(Card_Figure::KING, Card_Color::HEART);
   output.playerCards.emplace_back(Card_Figure::FOUR, Card_Color::HEART);
+  output.playerCards.emplace_back(Card_Figure::KING, Card_Color::HEART);
+
+  output.playerCards.emplace_back(Card_Figure::FOUR, Card_Color::SPADE);
+  output.playerCards.emplace_back(Card_Figure::SIX, Card_Color::SPADE);
+  output.playerCards.emplace_back(Card_Figure::SEVEN, Card_Color::SPADE);
+  output.playerCards.emplace_back(Card_Figure::TEN, Card_Color::SPADE);
+
+  output.playerCards.emplace_back(Card_Figure::FIVE, Card_Color::DIAMOND);
+  output.playerCards.emplace_back(Card_Figure::SIX, Card_Color::DIAMOND);
+
+  output.playerCards.emplace_back(Card_Figure::SIX, Card_Color::CLUB);
+  output.playerCards.emplace_back(Card_Figure::JACK, Card_Color::CLUB);
+
+  output.stackCard = Card(Card_Figure::EIGHT, Card_Color::HEART);
+  output.myMove = true;
   
   EXPECT_CALL(tableObserver, onUpdate(ContainsThisSnapshot(output)));
-  
   detectionSystem.run();
 }
--- test/SnapshotMatcher.hpp
+++ test/SnapshotMatcher.hpp
@@ -10,5 +10,8 @@ MATCHER_P(ContainsThisSnapshot, value, "") {
   auto stackCardIsNone = Card(Card_Figure::None, Card_Color::None) == value.stackCard;
   if(arg.stackCard != value.stackCard and not stackCardIsNone)
     return false;
+
+  if(arg.myMove != value.myMove)
+    return false;
   return true;
 }
