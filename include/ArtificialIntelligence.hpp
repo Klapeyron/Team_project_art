29f8422d5ee70a3beb67776433b144f6a4322ebc
Your Name <you@example.com>
2016-04-25 02:26:48 +0200

    add doing turn

--- include/ArtificialIntelligence.hpp
+++ include/ArtificialIntelligence.hpp
@@ -10,6 +10,10 @@
 #include "IGameControl.hpp"
 #include "TableObserver.hpp"
 
+#define MIN_SEQ_LEN (3)
+
+enum class StackType {SHOWN_STACK, HIDDEN_STACK};
+
 struct Comparation
 {
   bool operator()(const Card& a, const Card& b)
@@ -20,19 +24,25 @@ struct Comparation
 
 class ArtificialIntelligence :public TableObserver {
   IGameControl & gameControl;
-  const int minSeqLen = 3;
+  StackType lastAction;
+  Card lastTurn;
+  std::list < Card > ungruppedCards;//niepogrupowane karty
   std::list < Card > cardDeck;//talia kart
   std::multiset < Card, Comparation > cards;//karty do przeszukania
   std::vector < std::multiset < Card, Comparation > > sequences;//znalezione sekwencje
   std::vector < std::vector < Card > > groups;//znalezione grupy
+  std::vector < Card > opponentCards;//karty przeciwnika
   
  public:
   ArtificialIntelligence(IGameControl & gameControlInterface) :gameControl(gameControlInterface)
   {
+// 	  for(const auto& figure : Card_Figure())
+// 		  for(const auto& color : Card_Color())
     for(int figure = 1; figure <= static_cast<int>(Card_Figure::KING); ++figure)
       for(int color = 1; color <= static_cast<int>(Card_Color::DIAMOND); ++color)
-	cardDeck.push_back( Card( static_cast<Card_Figure> (figure), static_cast<Card_Color> (color)));
+		  cardDeck.push_back( Card( static_cast<Card_Figure> (figure), static_cast<Card_Color> (color)));
   }
+  
   void onUpdate(TableSnapshot const&);
   
   std::vector < std::multiset < Card, Comparation > > ReturnSeqs()
@@ -77,9 +87,13 @@ class ArtificialIntelligence :public TableObserver {
   
   bool DecisionToTakeTheCard(const Card& card);
   
-  void RemoveCardFromDeck(const Card& card);
+  int RemoveCardsFromDeck();
   
-  void CreateCards();
+  bool RemoveCardFromDeck(const Card& card);
   
-  void ShowCards();
+  int CreateCards();
+  
+  int ShowCards();
+  
+  int ShowCardDeck();
 };
--- src/ArtificialIntelligence.cpp
+++ src/ArtificialIntelligence.cpp
@@ -2,8 +2,14 @@
 
 void ArtificialIntelligence::onUpdate(TableSnapshot const& tableSnapshot)
 {
+	static bool firstTime = true;
 	if(tableSnapshot.myMove)
 	{
+		if(firstTime)
+		{
+			RemoveCardsFromDeck();
+			firstTime = false;
+		}
 		for (auto karta : tableSnapshot.playerCards)
 			cards.insert(karta);
 		AddFindSeqs();
@@ -11,11 +17,25 @@ void ArtificialIntelligence::onUpdate(TableSnapshot const& tableSnapshot)
 		if (DecisionToTakeTheCard(tableSnapshot.stackCard))
 		{
 			gameControl.pickCardFromStack();
+			lastAction = StackType::SHOWN_STACK;
 		}
 		else
 		{
 			gameControl.pickCardFromHiddenStack();
-                }
+			lastAction = StackType::HIDDEN_STACK;
+		}
+		std::multiset < Card, Comparation > tmpCards;
+		for (std::multiset < Card, Comparation >::iterator it = cards.begin(); it != cards.end(); ++it)
+		{
+			if(!FindCardInSeqs(*it, sequences) && !FindCardInGroups(*it, groups))
+				tmpCards.insert(*it);
+		}
+		if(tmpCards.size())
+		{
+			gameControl.throwMyCard(*tmpCards.begin());
+			if(tmpCards.size() == 1)
+				gameControl.endGame();
+		}
 	}
 	else
 		RemoveCardFromDeck(tableSnapshot.stackCard);
@@ -55,27 +75,27 @@ void ArtificialIntelligence::AddFindGroups()
 std::vector < std::vector < Card > > ArtificialIntelligence::FindGroups(const std::multiset < Card, Comparation >& source)
 {
 	std::vector < std::vector < Card > > groupsTmp;
-	std::vector< Card > tmp;
+	std::vector< Card > tmp;//grupa
 	for (std::multiset< Card >::iterator it = source.begin(); it != source.end(); ++it)
 	{
 		std::vector< Card >::iterator it2;
 		for (it2 = tmp.begin(); it2 != tmp.end(); ++it2)
 			if(it2->getFigure() != it->getFigure())
 				break;
-			if(it2 != tmp.end())
-			{
-				if(tmp.size() >= minSeqLen)
-					groupsTmp.push_back(tmp);
-				tmp.clear();
-			}
-			std::vector< Card >::iterator it3;
+		if(it2 != tmp.end())
+		{
+			if(tmp.size() >= MIN_SEQ_LEN)
+				groupsTmp.push_back(tmp);
+			tmp.clear();
+		}
+		std::vector< Card >::iterator it3;
 		for (it3 = tmp.begin(); it3 != tmp.end(); ++it3)
 			if(it3->getColor() == it->getColor())
 				break;
-			if(it3 == tmp.end())
-				tmp.push_back(*it);
+		if(it3 == tmp.end())
+			tmp.push_back(*it);
 	}
-	if(tmp.size() >= minSeqLen)
+	if(tmp.size() >= MIN_SEQ_LEN)
 		groupsTmp.push_back(tmp);
 	return groupsTmp;
 }
@@ -86,7 +106,7 @@ bool ArtificialIntelligence::FindCardInGroups(const Card& card, std::vector < st
 		for (std::vector< Card >::iterator it = it1->begin(); it != it1->end(); ++it)
 			if(*it == card)
 				return true;
-	return false;
+			return false;
 }
 
 void ArtificialIntelligence::AddFindSeqs()
@@ -107,36 +127,68 @@ std::vector < std::multiset < Card, Comparation > > ArtificialIntelligence::Find
 
 std::vector < std::multiset < Card, Comparation > > ArtificialIntelligence::FindOneColorSeq(Card_Color color, const std::multiset < Card, Comparation >& source)
 {
+	std::list < Card > ungruppedCardsTmp;
 	std::vector < std::multiset < Card, Comparation > > tmp;
 	int i = 0;
 	std::multiset < Card, Comparation > sequence;
 	//szukanie startu
 	for (std::multiset < Card, Comparation >::iterator it = source.begin(); it != source.end(); ++it)
-		if(it->getColor() == color)
+		if(it->getColor() == color)//przeszukiwanie danego koloru
 		{
 			i = static_cast<std::underlying_type<Card_Figure>::type>(it->getFigure());
 			break;
 		}
-		//szukanie sekwencji
-		for (std::multiset < Card, Comparation >::iterator it = source.begin(); it != source.end(); ++it)
+	//szukanie sekwencji
+	for (std::multiset < Card, Comparation >::iterator it = source.begin(); it != source.end(); ++it)
+	{
+		if(it->getColor() == color)
 		{
-			if(it->getColor() == color)
+			if(static_cast<std::underlying_type<Card_Figure>::type>(it->getFigure()) == i)
+				sequence.insert(Card(it->getFigure(), it->getColor()));
+			else
 			{
-				if(static_cast<std::underlying_type<Card_Figure>::type>(it->getFigure()) == i)
-					sequence.insert(Card(it->getFigure(), it->getColor()));
+				if(sequence.size() >= MIN_SEQ_LEN)
+					tmp.push_back(sequence);
 				else
 				{
-					if(sequence.size() >= minSeqLen)
-						tmp.push_back(sequence);
-					sequence.clear();
-					--it;
+					for (const Card card : sequence)
+						ungruppedCardsTmp.push_back(card);
 				}
-				if(++i > static_cast<int> (Card_Figure::KING)) break;
+				sequence.clear();
+				--it;
+			}
+			if(++i > static_cast<int> (Card_Figure::KING)) break;
+		}
+	}
+	if(sequence.size() >= MIN_SEQ_LEN)
+		tmp.push_back(sequence);
+	else
+	{
+		for (const Card card : sequence)
+			ungruppedCardsTmp.push_back(card);
+	}
+	
+	for(std::list < Card >::iterator it = ungruppedCards.begin(); it != ungruppedCards.end(); )
+	{
+		bool czyZanl = false;
+		for (std::vector < std::multiset < Card, Comparation > >::iterator it1 = sequences.begin(); it1 != sequences.end(); ++it1)
+		{
+			std::multiset < Card, Comparation >::iterator itf = find (it1->begin(), it1->end(), *it);
+			if(itf != it1->end())
+			{
+				czyZanl = true;
+				break;
 			}
 		}
-		if(sequence.size() >= minSeqLen)
-			tmp.push_back(sequence);
-		return tmp;
+		if(czyZanl)
+			it = ungruppedCards.erase(it);
+		else
+			++it;
+	}
+	if(!ungruppedCards.size())
+		ungruppedCards.insert(ungruppedCards.end(), ungruppedCardsTmp.begin(), ungruppedCardsTmp.end());
+	
+	return tmp;
 }
 
 bool ArtificialIntelligence::FindCardInSeq(const Card& card, const std::multiset < Card, Comparation >& sequence)
@@ -166,9 +218,9 @@ bool ArtificialIntelligence::DecisionToTakeTheCard(const Card& card)
 			if (suitableCardBegin == card)
 				return true;
 		}
-		if (it1->end()->getFigure() < Card_Figure::KING)//sprawdzanie początku
+		if (std::prev(it1->end())->getFigure() < Card_Figure::KING)//sprawdzanie początku
 		{
-                  Card suitableCardEnd = Card(static_cast<Card_Figure>(static_cast<int>(std::prev(it1->end())->getFigure()) + 1), std::prev(it1->end())->getColor());
+			Card suitableCardEnd = Card(static_cast<Card_Figure>(static_cast<int>(std::prev(it1->end())->getFigure()) + 1), std::prev(it1->end())->getColor());
 			if (suitableCardEnd == card)
 				return true;
 		}
@@ -192,23 +244,37 @@ bool ArtificialIntelligence::DecisionToTakeTheCard(const Card& card)
 			tmpCards.insert(*it);
 	}
 	tmpCards.insert(card);
-	if(!FindSeqs(tmpCards).empty() or !FindGroups(tmpCards).empty())
+	if(!FindSeqs(tmpCards).empty() || !FindGroups(tmpCards).empty())
 		return true;
 	
 	return false;
 }
 
-void ArtificialIntelligence::RemoveCardFromDeck(const Card& card)
+int ArtificialIntelligence::RemoveCardsFromDeck()
 {
-	for(std::list < Card >::iterator it = cardDeck.begin(); it != cardDeck.end(); ++it)
-		if(*it == card)
-                {
-			cardDeck.erase(it);
-                        break;
-                }
+	int l = 0;
+	for (const Card& card : cards)
+	{
+		if(ArtificialIntelligence::RemoveCardFromDeck(card))
+			++l;
+	}
+	return l;
 }
 
-void ArtificialIntelligence::CreateCards()
+bool ArtificialIntelligence::RemoveCardFromDeck(const Card& card)
+{
+	bool isRemoved = false;
+	for(std::list < Card >::iterator it = cardDeck.begin(); it != cardDeck.end(); ++it)
+		if(*it == card)
+		{
+			cardDeck.erase(it);
+			isRemoved = true;
+			break;
+		}
+	return isRemoved;
+}
+
+int ArtificialIntelligence::CreateCards()
 {
 	cards.insert(Card(Card_Figure::THREE, Card_Color::SPADE));
 	cards.insert(Card(Card_Figure::THREE, Card_Color::CLUB));
@@ -227,13 +293,22 @@ void ArtificialIntelligence::CreateCards()
 	cards.insert(Card(Card_Figure::FIVE, Card_Color::SPADE));
 	cards.insert(Card(Card_Figure::SEVEN, Card_Color::DIAMOND));
 	cards.insert(Card(Card_Figure::SEVEN, Card_Color::HEART));
+	
+	return cards.size();
 }
 
-void ArtificialIntelligence::ShowCards()
+int ArtificialIntelligence::ShowCards()
 {
 	std::cout << "Cards:" << std::endl;
-	for (std::multiset< Card >::iterator it = cards.begin(); it != cards.end(); ++it)
-	{
-		std::cout << "f: " << static_cast<std::underlying_type<Card_Figure>::type>(it->getFigure()) << " c: " << static_cast<std::underlying_type<Card_Color>::type>(it->getColor()) << std::endl;
-	}
+	for (const Card& card : cards)
+		std::cout << "f: " << static_cast<std::underlying_type<Card_Figure>::type>(card.getFigure()) << " c: " << static_cast<std::underlying_type<Card_Color>::type>(card.getColor()) << std::endl;
+	return cards.size();
+}
+
+int ArtificialIntelligence::ShowCardDeck()
+{
+	std::cout << "Card Deck:" << std::endl;
+	for (const Card& card : cardDeck)
+		std::cout << "f: " << static_cast<std::underlying_type<Card_Figure>::type>(card.getFigure()) << " c: " << static_cast<std::underlying_type<Card_Color>::type>(card.getColor()) << std::endl;
+	return cardDeck.size();
 }
