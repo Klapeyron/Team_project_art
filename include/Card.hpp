41100e9468a334ca5255d4aefe1705019239e5fd
Klapeyron <t.m@o2.pl>
2016-04-25 10:42:31 +0200

    Card operators refactor

--- include/Card.hpp
+++ include/Card.hpp
@@ -6,14 +6,14 @@
 enum class Card_Color : unsigned int {None = 0, SPADE, HEART, CLUB, DIAMOND}; 
 enum class Card_Figure : unsigned int {None = 0, A, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
 
-const inline Card_Color operator++(Card_Color x) { return static_cast<Card_Color>(static_cast<unsigned int>(x) + 1); }
-const inline Card_Color operator--(Card_Color x) { return static_cast<Card_Color>(static_cast<unsigned int>(x) - 1); }
+const inline Card_Color operator++(Card_Color & x) { return x = static_cast<Card_Color>(static_cast<unsigned int>(x) + 1); }
+const inline Card_Color operator--(Card_Color & x) { return x = static_cast<Card_Color>(static_cast<unsigned int>(x) - 1); }
 const inline Card_Color operator*(Card_Color x) {return x;}
 const inline Card_Color begin(Card_Color x) {return Card_Color::SPADE;}
 const inline Card_Color end(Card_Color x) {return static_cast<Card_Color>(static_cast<unsigned int>(Card_Color::DIAMOND) + 1);}
 
-const inline Card_Figure operator++(Card_Figure x) {return static_cast<Card_Figure>(static_cast<unsigned int>(x) + 1);}
-const inline Card_Figure operator--(Card_Figure x) {return static_cast<Card_Figure>(static_cast<unsigned int>(x) - 1);}
+const inline Card_Figure operator++(Card_Figure & x) { return x = static_cast<Card_Figure>(static_cast<unsigned int>(x) + 1); }
+const inline Card_Figure operator--(Card_Figure & x) { return x = static_cast<Card_Figure>(static_cast<unsigned int>(x) - 1); }
 const inline Card_Figure operator*(Card_Figure x) {return x;}
 const inline Card_Figure begin(Card_Figure x) {return Card_Figure::A;}
 const inline Card_Figure end(Card_Figure x) {return static_cast<Card_Figure>(static_cast<unsigned int>(Card_Figure::KING) + 1);}
