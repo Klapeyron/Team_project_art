a35d44e8978fe8ec1ad3b556272a720f8d04d573
klapeyron <t.m@o2.pl>
2016-05-02 08:56:23 +0200

    1680x1050 detection system test

--- test/DetectionSystemTest.cpp
+++ test/DetectionSystemTest.cpp
@@ -7,7 +7,7 @@
 
 TEST (DetectionSystemTest, 1366x768DetectionTest)
 {
-  const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/tableSnapshot1366x768.tiff";
+  const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/tableSnapshot1366x768.png";
   DetectionSystem detectionSystem (tableSnapshotFilePath);
   
   TableObserverMock tableObserver;
@@ -29,6 +29,36 @@ TEST (DetectionSystemTest, 1366x768DetectionTest)
   output.playerCards.emplace_back(Card_Figure::JACK, Card_Color::CLUB);
 
   output.stackCard = Card(Card_Figure::EIGHT, Card_Color::HEART);
+  output.myMove = true;
+  
+  EXPECT_CALL(tableObserver, onUpdate(ContainsThisSnapshot(output)));
+  detectionSystem.processTable();
+}
+
+TEST (DetectionSystemTest, 1680x1050DetectionTest)
+{
+  const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/tableSnapshot1680x1050.png";
+  DetectionSystem detectionSystem (tableSnapshotFilePath);
+  
+  TableObserverMock tableObserver;
+  detectionSystem.registerOberver(tableObserver);
+ 
+  TableSnapshot output;
+  output.playerCards.emplace_back(Card_Figure::A, Card_Color::DIAMOND);
+  output.playerCards.emplace_back(Card_Figure::A, Card_Color::SPADE);
+
+  output.playerCards.emplace_back(Card_Figure::TWO, Card_Color::CLUB);
+  output.playerCards.emplace_back(Card_Figure::THREE, Card_Color::CLUB);
+  output.playerCards.emplace_back(Card_Figure::FIVE, Card_Color::DIAMOND);
+  output.playerCards.emplace_back(Card_Figure::SIX, Card_Color::SPADE);
+  output.playerCards.emplace_back(Card_Figure::SEVEN, Card_Color::SPADE);
+
+  output.playerCards.emplace_back(Card_Figure::EIGHT, Card_Color::CLUB);
+  output.playerCards.emplace_back(Card_Figure::NINE, Card_Color::CLUB);
+  
+  output.playerCards.emplace_back(Card_Figure::KING, Card_Color::CLUB);
+
+  output.stackCard = Card(Card_Figure::FOUR, Card_Color::DIAMOND);
   output.myMove = true;
   
   EXPECT_CALL(tableObserver, onUpdate(ContainsThisSnapshot(output)));
