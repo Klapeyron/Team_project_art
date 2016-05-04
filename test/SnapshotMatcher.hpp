6343f7646b91d883b625459a2fc13bf0deb44a65
Klapeyron <t.m@o2.pl>
2016-05-04 10:03:41 +0200

    Changet snapshot matching method

--- test/SnapshotMatcher.hpp
+++ test/SnapshotMatcher.hpp
@@ -2,19 +2,7 @@
 
 #include "gtest/gtest.h"
 
-MATCHER_P(ContainsThisSnapshot, value, "") {
-  for(auto const& card : value.playerCards)
-  {
-    auto cardFound = std::find(arg.playerCards.begin(), arg.playerCards.end(), card) != arg.playerCards.end();
-    if( not cardFound )
-      return false;
-  }
-
-  auto stackCardIsNone = Card(Card_Figure::None, Card_Color::None) == value.stackCard;
-  if(arg.stackCard != value.stackCard and not stackCardIsNone)
-    return false;
-
-  if(arg.myMove != value.myMove)
-    return false;
-  return true;
+MATCHER_P(ContainsThisSnapshot, value, "")
+{
+  return value == arg;
 }
