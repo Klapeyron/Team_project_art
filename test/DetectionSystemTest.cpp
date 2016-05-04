f3a52805c1435d02b27b1989c91eff0c3129b42c
Klapeyron <t.m@o2.pl>
2016-05-04 10:04:29 +0200

    Move tests for Deteection system

--- test/DetectionSystemTest.cpp
+++ test/DetectionSystemTest.cpp
@@ -9,10 +9,10 @@ TEST (DetectionSystemTest, 1366x768DetectionTest)
 {
   const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/tableSnapshot1366x768.png";
   DetectionSystem detectionSystem (tableSnapshotFilePath);
-  
+
   TableObserverMock tableObserver;
   detectionSystem.registerOberver(tableObserver);
- 
+
   TableSnapshot output;
   output.playerCards.emplace_back(Card_Figure::TWO, Card_Color::HEART);
   output.playerCards.emplace_back(Card_Figure::THREE, Card_Color::HEART);
@@ -23,14 +23,14 @@ TEST (DetectionSystemTest, 1366x768DetectionTest)
 
   output.playerCards.emplace_back(Card_Figure::FIVE, Card_Color::DIAMOND);
   output.playerCards.emplace_back(Card_Figure::KING, Card_Color::DIAMOND);
-  
+
   output.playerCards.emplace_back(Card_Figure::FIVE, Card_Color::SPADE);
   output.playerCards.emplace_back(Card_Figure::EIGHT, Card_Color::SPADE);
   output.playerCards.emplace_back(Card_Figure::QUEEN, Card_Color::SPADE);
 
   output.stackCard = Card(Card_Figure::JACK, Card_Color::SPADE);
   output.myMove = true;
-  
+
   EXPECT_CALL(tableObserver, onUpdate(ContainsThisSnapshot(output)));
   detectionSystem.processTable();
   detectionSystem.waitForUnfinishedJobs();
@@ -40,10 +40,10 @@ TEST (DetectionSystemTest, 1680x1050DetectionTest)
 {
   const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/tableSnapshot1680x1050.png";
   DetectionSystem detectionSystem (tableSnapshotFilePath);
-  
+
   TableObserverMock tableObserver;
   detectionSystem.registerOberver(tableObserver);
- 
+
   TableSnapshot output;
   output.playerCards.emplace_back(Card_Figure::TWO, Card_Color::CLUB);
   output.playerCards.emplace_back(Card_Figure::THREE, Card_Color::CLUB);
@@ -53,14 +53,46 @@ TEST (DetectionSystemTest, 1680x1050DetectionTest)
 
   output.playerCards.emplace_back(Card_Figure::A, Card_Color::DIAMOND);
   output.playerCards.emplace_back(Card_Figure::FIVE, Card_Color::DIAMOND);
-  
+
   output.playerCards.emplace_back(Card_Figure::A, Card_Color::SPADE);
   output.playerCards.emplace_back(Card_Figure::SIX, Card_Color::SPADE);
   output.playerCards.emplace_back(Card_Figure::SEVEN, Card_Color::SPADE);
-  
+
   output.stackCard = Card(Card_Figure::FOUR, Card_Color::DIAMOND);
   output.myMove = true;
-  
+
+  EXPECT_CALL(tableObserver, onUpdate(ContainsThisSnapshot(output)));
+  detectionSystem.processTable();
+  detectionSystem.waitForUnfinishedJobs();
+}
+
+TEST (DetectionSystemTest, enemyCardTakenTest_customSize)
+{
+  const std::string tableSnapshotFilePath = "../test/DetectionSystemImages/enemyMove_customSize.png";
+  DetectionSystem detectionSystem (tableSnapshotFilePath);
+
+  TableObserverMock tableObserver;
+  detectionSystem.registerOberver(tableObserver);
+
+  TableSnapshot output;
+  output.playerCards.emplace_back(Card_Figure::THREE, Card_Color::HEART);
+  output.playerCards.emplace_back(Card_Figure::FIVE, Card_Color::HEART);
+
+  output.playerCards.emplace_back(Card_Figure::TWO, Card_Color::CLUB);
+  output.playerCards.emplace_back(Card_Figure::NINE, Card_Color::CLUB);
+  output.playerCards.emplace_back(Card_Figure::JACK, Card_Color::CLUB);
+  output.playerCards.emplace_back(Card_Figure::KING, Card_Color::CLUB);
+
+  output.playerCards.emplace_back(Card_Figure::NINE, Card_Color::DIAMOND);
+
+  output.playerCards.emplace_back(Card_Figure::A, Card_Color::SPADE);
+  output.playerCards.emplace_back(Card_Figure::TEN, Card_Color::SPADE);
+  output.playerCards.emplace_back(Card_Figure::JACK, Card_Color::SPADE);
+
+  output.stackCard = Card(Card_Figure::A, Card_Color::DIAMOND);
+  output.myMove = false;
+  output.opponentTookCardFromHiddenStack = true;
+
   EXPECT_CALL(tableObserver, onUpdate(ContainsThisSnapshot(output)));
   detectionSystem.processTable();
   detectionSystem.waitForUnfinishedJobs();
