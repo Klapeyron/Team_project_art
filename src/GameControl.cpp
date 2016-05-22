3f81d0b2823703ea6a06fd755614d59e4f1f8d09
209255 <209255@student.pwr.edu.pl>
2016-05-22 18:05:29 +0200

    change xlib to xdotool

--- src/GameControl.cpp
+++ src/GameControl.cpp
@@ -59,59 +59,10 @@ void GameControl::pressOK()
 
 void GameControl::setPosition(const Position & position)
 {
- Display *display = XOpenDisplay(0);
- Window root = DefaultRootWindow(display);
- XWarpPointer(display, None, root, 0, 0, 0, 0, position.getX(), position.getY());
- mouseClick(Button1);
+  std::string x = std::to_string(position.getX());
+  std::string y = std::to_string(position.getY());
+  std::string br = " ";
+  std::string cmd = "xdotool mousemove "+x+br+y;
+  system(cmd.c_str());
+  system("xdotool click 1");
 }
-
-void GameControl::mouseClick(int button)
-{
-    Display *display = XOpenDisplay(0);
-
-    XEvent event;
-
-    if(display == NULL)
-    {
-      std::cerr<< "Error" << std::endl;
-      exit(-1);
-    }
-
-    memset(&event, 0x00, sizeof(event));
-
-    event.type = ButtonPress;
-    event.xbutton.button = button;
-    event.xbutton.same_screen = True;
-
-    XQueryPointer(display, RootWindow(display, DefaultScreen(display)), 
-		  &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, 
-		  &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-
-    event.xbutton.subwindow = event.xbutton.window;
-
-    while(event.xbutton.subwindow)
-    {
-        event.xbutton.window = event.xbutton.subwindow;
-
-        XQueryPointer(display, event.xbutton.window, &event.xbutton.root, 
-		      &event.xbutton.subwindow, &event.xbutton.x_root, 
-		      &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-    }
-
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) 
-      std::cerr << "Error\n" << std::endl;
-
-    XFlush(display);
-
-    usleep(100000);
-
-    event.type = ButtonRelease;
-    event.xbutton.state = 0x100;
-
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) 
-      std::cerr << "Error\n" << std::endl;
-
-    XFlush(display);
-    XCloseDisplay(display);
-}
-
